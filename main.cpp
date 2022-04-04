97d8052c25f18e3fc4daaa722f25e3e139fa8fc4
DanielNduati <dannduati2@gmail.com>
2022-04-04 15:36:14 +0300

    Update main.cpp
    Add and use add() function from the adder module

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
 #include <iostream>
-
-int main(int argc, char** argv){std::cout<<"Hello cmake!"<<std::endl;return 0;}
+float add(float a,float b);
+int main(int argc, char** argv){
+    std::cout<<"Hello cmake!\n";
+    std::cout<<add(69.420f,420.69f)<<"\n";
+    return 0;
+}
