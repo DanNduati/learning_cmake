a68110710780e087455ecc7dcdf84edcad024870
DanielNduati <dannduati2@gmail.com>
2022-04-07 06:39:38 +0300

    Update main.cpp
    Add adde header file containing the add function definition

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-float add(float a,float b);
+#include "Adder/adder.h"
+
 int main(int argc, char** argv){
     std::cout<<"Hello cmake!\n";
     std::cout<<add(69.420f,420.69f)<<"\n";
